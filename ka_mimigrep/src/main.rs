46ba0ade924dc24562283023718b3278056e8b2e
machine <machineswift@163.com>
2019-12-03 18:22:47 +0800

    ka_mimigrep

---
+++ ka_mimigrep/src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::process;
+use ka_mimigrep::Config;
+use ka_mimigrep::run;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
